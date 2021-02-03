abba18949c107ed7924f5fef9210922349892bf6
AAKANKSHA-DEKATE <aakanksha.dekate@appxbuild.com>
2021-02-03 17:13:10 +0530

    commit on master

--- React/Crash_Course_React/src/App.js
+++ React/Crash_Course_React/src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import './App.css';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
 
 const App = () => {
-  return React.createElement('h1', {}, 'Hi, this is React');
-};
+  const courseGoals = [
+    {id: 'cg1', text: 'Finish the course'},
+    {id: 'cg2', text: 'Learn all about the course Main Topics'},
+    {id: 'cg3', text: 'Help other students in the course'}
+  ];
+
+  return (
+    <div className= "course-goals">
+      <h2>Course Goals</h2>
+      <NewGoal />
+      <GoalList goals = {courseGoals}/>
+    </div>
+  );
+}; // This is a React based functional component. Modern way to use React.
+
+// class App extends React.Component {
+//   render() {
+//     return <h1 title="This works!">Hi, this is ReactJS!</h1>
+//   }
+// } // This is a regular React Component. Both display the same output.
 
 export default App;
